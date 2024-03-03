6de79666ef967ecd2f2ca010044d3144f78f0d0f
MzAymen <mezghanimohamedaymen@gmail.com>
2024-03-03 23:12:49 +0100

    add access to front

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: 'http://127.0.0.1:5500', // Replace with your frontend URL
+    methods: 'GET,POST,DELETE,PUT',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  };
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
