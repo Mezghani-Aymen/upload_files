dd1c416631589fd5d1c44d0e5eca18802e47ca54
MzAymen <mezghanimohamedaymen@gmail.com>
2024-03-03 11:49:59 +0100

    update upload_file.module
    save file in folder

--- src/upload_file/upload_file.module.ts
+++ src/upload_file/upload_file.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UploadFileController } from './upload_file.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  controllers: [UploadFileController]
+  imports: [
+    MulterModule.register({
+      dest: './upload',
+    }),
+  ],
+  controllers: [UploadFileController],
 })
 export class UploadFileModule {}
