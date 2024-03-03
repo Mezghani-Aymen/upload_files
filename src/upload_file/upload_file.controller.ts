da0f4bd16e5d5cb35abc5e75fa90779d8136179a
MzAymen <mezghanimohamedaymen@gmail.com>
2024-03-03 11:37:51 +0100

    update upload_file.controller

--- src/upload_file/upload_file.controller.ts
+++ src/upload_file/upload_file.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('upload-file')
-export class UploadFileController {}
+export class UploadFileController {
+  constructor() {}
+
+  @Post()
+  uploadimage() {}
+}
