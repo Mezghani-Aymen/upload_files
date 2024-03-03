d1307a5cfa6435263a3843d4d4e5593dd22f8a05
MzAymen <mezghanimohamedaymen@gmail.com>
2024-03-03 12:04:49 +0100

    update upload_file.module

--- src/upload_file/upload_file.module.ts
+++ src/upload_file/upload_file.module.ts
@@ -1,11 +1,34 @@
 import { Module } from '@nestjs/common';
 import { UploadFileController } from './upload_file.controller';
 import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import * as path from 'path';
 
 @Module({
   imports: [
     MulterModule.register({
-      dest: './upload',
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          // get name of file
+          const originalname = file.originalname;
+
+          // get extension (jpg-png-.....)
+          const extension = path.extname(originalname);
+
+          // create unique id
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+
+          // generate new name
+          const filename =
+            path.basename(originalname, extension) +
+            '-' +
+            uniqueSuffix +
+            extension;
+          callback(null, filename);
+        },
+      }),
     }),
   ],
   controllers: [UploadFileController],
