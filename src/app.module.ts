57f667299b1fbb8f749c045856c6ccd9a4edf2b6
MzAymen <mezghanimohamedaymen@gmail.com>
2024-03-10 21:42:03 +0100

    [add] controller user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadFileModule } from './upload_file/upload_file.module';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [UploadFileModule],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
